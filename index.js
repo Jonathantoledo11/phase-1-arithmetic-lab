f5239fb908f3719066d03459c36cce67cecea898
Jonathan <jonathantoledo11@gmail.com>
2021-11-03 10:18:03 -0700

    commit tree aa0354bd112053865557ac2e16c4ca4df872bd85

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+
+let multiply = num1 * num2; {
+    console.log(multiply);
+
+}
+
+let num3 = 4;
+let num4 = 1;
+
+let mod = num3 / num4; {
+    console.log(mod);
+}
+
+let set1 = new Set ([5, 10, 15, 20]);
+let max = 20; {
+    console.log(max);
+}
+
+let num5 = 5;
+let random = num5; {
+    console.log(random);
+}
+
